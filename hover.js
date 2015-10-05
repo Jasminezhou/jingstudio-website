ed7657d80cf6696e8112e5ab12a9cc7487e57ebf
jasmine <jasminezhou27@gmail.com>
2015-10-05 13:10:17 -0700

    good start

---
+++ hover.js
@@ -0,0 +1,59 @@
+
+
+$(document).ready(function(){
+    $(".details").hover(function(){
+        $(".case-title").addClass("move-up");
+		}, function() {
+  		$(".case-title").removeClass("move-up");
+    });
+});
+
+
+$(document).ready(function(){
+    $(".details").hover(function(){
+        $(".case-details").addClass("move-down");
+		}, function() {
+  		$(".case-details").removeClass("move-down");
+    });
+});
+
+
+$(document).ready(function(){
+    $(".details").hover(function(){
+        $(".case-img").addClass("show-img");
+		}, function() {
+  		$(".case-img").removeClass("show-img");
+    });
+});
+
+
+$(document).ready(function(){
+    $(".details").hover(function(){
+        $(".case-img-device").addClass("show-img");
+    }, function() {
+      $(".case-img-device").removeClass("show-img");
+    });
+});
+
+
+$(document).ready(function(){
+    $(".details").hover(function(){
+        $(".bg").removeClass("bg-white");
+    }, function() {
+      $(".bg").addClass("bg-white");
+    });
+});
+
+
+
+
+$(document).ready(function(){
+    $(".case-title").hover(function(){
+        $(".bg").removeClass("bg-white");
+    }, function() {
+      $(".bg").addClass("bg-white");
+    });
+});
+
+
+
