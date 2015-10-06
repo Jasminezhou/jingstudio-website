1411c6c3c2e0c94bda0357ab170c339153ca8b30
jasmine <jasminezhou27@gmail.com>
2015-10-06 04:04:45 -0700

    created diamond carousel-indicator

--- hover.js
+++ hover.js
@@ -27,11 +27,53 @@ $(document).ready(function(){
 
 $(document).ready(function(){
     $(".details").hover(function(){
+        $(".case-details").addClass("white");
+    }, function() {
+      $(".case-details").removeClass("white");
+    });
+});
+
+$(document).ready(function(){
+    $(".details").hover(function(){
         $(".bg").removeClass("bg-white");
     }, function() {
       $(".bg").addClass("bg-white");
     });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+    $(".fa-arrow-right").hover(function(){
+        $("#indicator").addClass("rotateY-right");
+    }, function() {
+      $("#indicator").removeClass("rotateY-right");
+    });
+});
+$(document).ready(function(){
+    $(".fa-arrow-left").hover(function(){
+        $("#indicator").addClass("rotateY-left");
+    }, function() {
+      $("#indicator").removeClass("rotateY-left");
+    });
+});
+
+
 
 
 
