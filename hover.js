4fedad4ac112885683e53d83b36cb69871989853
jasmine <jasminezhou27@gmail.com>
2015-10-08 10:45:20 -0700

    menu re-organizing

--- hover.js
+++ hover.js
@@ -1,52 +1,32 @@
 $(document).ready(function(){
     $(".details").hover(function(){
-        $(".case-title").addClass("move-up");
-		}, function() {
-  		$(".case-title").removeClass("move-up");
+        $(".case-img-device").toggleClass("show-img");
+        $(".case-title").toggleClass("case-title-move-up");
+        $(".case-details").toggleClass("case-details-move");
+        $(".case-details").toggleClass("white");
+        $(".bg").toggleClass("bg-white");
     });
-});
 
 
-$(document).ready(function(){
-    $(".details").hover(function(){
-        $(".case-details").addClass("move-down");
-		}, function() {
-  		$(".case-details").removeClass("move-down");
+  $(".logo").hover(function(){
+        $(".brand").toggleClass("invisibile");
+        $(".logo span").toggleClass("menu-text");
     });
-});
 
 
-$(document).ready(function(){
-    $(".details").hover(function(){
-        $(".case-img-device").addClass("show-img");
-    }, function() {
-      $(".case-img-device").removeClass("show-img");
+    $(".case-title").hover(function(){
+        $(".bg").toggleClass("bg-white");
+        $(".case-details").toggleClass("white");
     });
-});
 
 
-$(document).ready(function(){
-    $(".details").hover(function(){
-        $(".case-details").addClass("white");
-    }, function() {
-      $(".case-details").removeClass("white");
+    $(".menu").hover(function(){
+        $(".menu i").toggleClass("menu-move");
+        $(".menu i").toggleClass("white");
+        $(".menu span").toggleClass("menu-text");
     });
+
 });
-
-$(document).ready(function(){
-    $(".details").hover(function(){
-        $(".bg").removeClass("bg-white");
-    }, function() {
-      $(".bg").addClass("bg-white");
-    });
-});
-
-
-
-
-
-
-
 
 
 
@@ -60,12 +40,14 @@ $(document).ready(function(){
 
 $(document).ready(function(){
     $(".fa-arrow-right").hover(function(){
-        $("#indicator").addClass("rotateY-right");
+        $("#indicator").toggleClass("rotateY-right");
     }, function() {
       $("#indicator").removeClass("rotateY-right");
     });
-});
-$(document).ready(function(){
+
+
+
+
     $(".fa-arrow-left").hover(function(){
         $("#indicator").addClass("rotateY-left");
     }, function() {
@@ -77,29 +59,4 @@ $(document).ready(function(){
 
 
 
-
-
-
-
-
-
-
-
-
-
-$(document).ready(function(){
-    $(".case-title").hover(function(){
-        $(".bg").removeClass("bg-white");
-    }, function() {
-      $(".bg").addClass("bg-white");
-    });
-});
-
-$(document).ready(function(){
-    $(".case-title").click(function () {
-          $(".case-title").addClass("move-up");
-          $(".case-details").addClass("move-down");
-          $(".case-img-device").addClass("show-img");
-    });
-     });
 
